86df68a92e2cb7c02f3388313b2f67be13c5f862
Leonard Laput <online.lorno+dsl@gmail.com>
2022-08-28 01:35:06 +0800

    ✨ UAM auth integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,77 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
+type UAMParams = {
+  res?: string
+  uamip?: string
+  uamport?: string
+  challenge?: string
+  called?: string
+  mac?: string
+  ip?: string
+  nasid?: string
+  sessionid?: string
+  userurl?: string
+}
+
 function App() {
-  const [auth, setauth] = useState(false)
+  //<login_url>
+  // &res=<uam_handshake_state>
+  // &uamip=<uam_ip_address>
+  // &uamport=<uam_port>
+  // &challenge=<chap_challenge_string>
+  // &called=<called_sta_id>
+  // &mac=<calling_sta_id>
+  // &ip=<client_ip_address>
+  // &nasid=<nas_gateway_id>
+  // &sessionid=<session_id>
+  // &userurl=<hotspot_detect_url>
+  const [loading, setLoading] = useState<boolean>(false)
+  const [uamParams, setUamParams] = useState<UAMParams | null>(null)
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search)
+    const params = Object.fromEntries(searchParams)
+    console.log({ params })
+    setUamParams(params as UAMParams)
+  }, [])
+
+  useEffect(() => {
+    console.log({ uamParams })
+  }, [uamParams])
+
+  const handleAuthticate = () => {
+    if (
+      uamParams &&
+      uamParams?.uamip &&
+      uamParams?.uamport &&
+      uamParams?.mac &&
+      uamParams?.challenge
+    ) {
+      setLoading(true)
+      window.location.replace(
+        `http://${uamParams?.uamip}:${uamParams?.uamport}/logon?username=${uamParams?.mac}&response=${uamParams?.challenge}`,
+      )
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Otterdev: Cradlepoint UAM Authentication</p>
         <p>
-          <button type="button" disabled={auth} onClick={() => setauth(true)}>
-            Authenticate
-          </button>
+          {uamParams &&
+          uamParams?.uamip &&
+          uamParams?.uamport &&
+          uamParams?.mac &&
+          uamParams?.challenge ? (
+            <button type="button" disabled={loading} onClick={handleAuthticate}>
+              Authenticate
+            </button>
+          ) : (
+            <span>Invalid Login URL</span>
+          )}
         </p>
       </header>
     </div>
